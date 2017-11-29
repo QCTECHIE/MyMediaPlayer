a75191b964fcb02a2fdd8db110c4d515c4211a7f
QCTECHIE <182307268@qq.com>
2017-11-29 09:43:29 +0800

    Initial commit

---
+++ app/src/main/java/com/csist/class16cchun/mymediaplayer/SplashActivity.java
@@ -0,0 +1,56 @@
+package com.csist.class16cchun.mymediaplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.Transformation;
+import android.view.animation.TranslateAnimation;
+import android.widget.RelativeLayout;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private RelativeLayout mRlRoot;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        //绑定控件
+        mRlRoot = findViewById(R.id.rl_root);
+
+        //旋转动画
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF
+                , 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setDuration(5000);
+        rotateAnimation.setFillAfter(true);
+
+        //渐变
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0, 1);
+        alphaAnimation.setDuration(5000);
+        alphaAnimation.setFillAfter(true);
+
+        //缩放动画
+        ScaleAnimation scaleAnimation = new ScaleAnimation(0, 1, 0, 1, Animation.RELATIVE_TO_SELF
+                , 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(5000);
+        scaleAnimation.setFillAfter(true);
+        //移动动画
+        TranslateAnimation translateAnimation = new TranslateAnimation(-250, 0,
+                -250, 0);
+        translateAnimation.setDuration(5000);
+        translateAnimation.setFillAfter(true);
+
+        //动画合集
+        AnimationSet set = new AnimationSet(true);
+        set.addAnimation(rotateAnimation);
+        set.addAnimation(alphaAnimation);
+        set.addAnimation(scaleAnimation);
+        set.addAnimation(translateAnimation);
+
+        mRlRoot.startAnimation(set);
+    }
+}
